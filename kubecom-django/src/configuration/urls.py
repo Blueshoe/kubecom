65292fff3f193ff564fa8c7ca778d8db2b8f093f
Veit Rückert <veit@blueshoe.de>
2022-07-05 16:01:28 +0200

    fix url import

--- kubecom-django/src/configuration/urls.py
+++ kubecom-django/src/configuration/urls.py
@@ -16,11 +16,9 @@ from django.contrib import admin
 from django.contrib.admindocs import urls as admindocs_urls
 from django.urls import include, path
 
-from apps.kubecom.urls import urls as kubecom_urls
-
 urlpatterns = [
     # django-admin:
     path("admin/doc/", include(admindocs_urls)),  # noqa: DJ05
     path("admin/", admin.site.urls),
-    path("/", include(kubecom_urls)),
+    path("/", include("apps.kubecom.urls")),
 ]
