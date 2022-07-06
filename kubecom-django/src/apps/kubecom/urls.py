defeed62b2bd7818a0e0153f6e2ec4ea27352503
Veit Rückert <veit@blueshoe.de>
2022-07-06 09:08:40 +0200

    fix view names add messages o main template

--- kubecom-django/src/apps/kubecom/urls.py
+++ kubecom-django/src/apps/kubecom/urls.py
@@ -4,6 +4,6 @@ from apps.kubecom.views import CustomResourceDefinitionView, HomeView, JobView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("job/", JobView.as_view(), name="job"),
-    path("crd/", CustomResourceDefinitionView.as_view(), name="crd"),
+    path("job", JobView.as_view(), name="job"),
+    path("crd", CustomResourceDefinitionView.as_view(), name="crd"),
 ]
